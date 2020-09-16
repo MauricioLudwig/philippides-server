05f9d424afe5162ca144005ad3995e4210e7bd67
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2020-09-16 02:13:00 +0200

    logging + shutdown + refactoring

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,26 +1,34 @@
 import socketio from 'socket.io';
 
 import { IMessageRequest, SocketType } from './definitions';
+import { logger } from './logger';
 import { Message } from './message';
 import { isMessageRequest } from './validator';
 import { io } from '../app';
 
 export const config = (socket: socketio.Socket): void => {
   io.on('connection', () => {
-    console.log('new connection');
+    logger.info(`New connection established, socket id: ${socket.id}`);
+    io.emit(SocketType.NewMessage, new Message().userConnected());
   });
 
   socket.on(SocketType.NewMessage, (req: unknown) => {
     if (!isMessageRequest(req)) {
-      // TODO send back validation error
+      logger.error('Invalid data provided');
     }
 
-    const { id, message } = req as IMessageRequest;
-    const newMessage = new Message().new(id, message);
-    socket.broadcast.emit(SocketType.NewMessage, newMessage);
+    logger.info('New message received');
+    const { id, text } = req as IMessageRequest;
+    const newMessage = new Message().new(id, text);
+    io.sockets.emit(SocketType.NewMessage, newMessage);
+    logger.info('New message sent');
   });
 
-  socket.on(SocketType.UserDisconnected, () => {});
+  socket.on(SocketType.UserDisconnected, () => {
+    logger.info('User disconnected');
+  });
 
-  socket.on(SocketType.UserInactive, () => {});
+  socket.on(SocketType.UserInactive, () => {
+    logger.info('User inactive');
+  });
 };
